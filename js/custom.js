af6054d238aab05e543a89f3b7cfea71d90eda9e
Dylan Rakotomahefa <dylan@unistra.fr>
2025-03-28 22:04:04 +0100

    🚧 Nettoyage de mon répertoire basé sur la version de Nilaina

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,29 @@
-
 function InitNavbarAnimation() {
-    window.addEventListener("scroll", function () {
-        let navbar = document.getElementById("navbar");
-        let header_container = document.querySelector(".header .container");
-        
-        if (window.scrollY > 70) {
-            if(this.window.innerWidth < 768) {
-                navbar.classList.remove("sticky-nav");
-                header_container.classList.add("container-mobile");
-                navbar.classList.add("navbar-mobile");
-            } else {
-                navbar.classList.add("sticky-nav");
-            }
-        } else {
-            if(this.window.innerWidth < 768) {
-                header_container.classList.remove("container-mobile");
-                navbar.classList.remove("navbar-mobile");
-            } else {
-                navbar.classList.remove("sticky-nav");
-            }
-        }
+  window.addEventListener("scroll", function () {
+    let navbar = document.getElementById("navbar");
+    let header_container = document.querySelector(".header .container");
 
-    });
+    if (window.scrollY > 70) {
+      if (this.window.innerWidth < 768) {
+        navbar.classList.remove("sticky-nav");
+        header_container.classList.add("container-mobile");
+        navbar.classList.add("navbar-mobile");
+      } else {
+        navbar.classList.add("sticky-nav");
+      }
+    } else {
+      if (this.window.innerWidth < 768) {
+        header_container.classList.remove("container-mobile");
+        navbar.classList.remove("navbar-mobile");
+      } else {
+        navbar.classList.remove("sticky-nav");
+      }
+    }
+  });
 }
 
 function initialize() {
-    InitNavbarAnimation();
+  InitNavbarAnimation();
 }
 
 // Exécution de la fonction initialize après le chargement complet du DOM
